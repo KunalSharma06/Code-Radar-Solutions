daad8fde685461d4730b09f879677d54b30abfec
Kunal Sharma <sharmakunal19806@gmail.com>
2025-03-20 23:00:43 +0530

    Solution to Find the Second Smallest Element in an Array

--- Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
+++ Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
@@ -1,59 +1,27 @@
 #include<stdio.h>
-// int check(int arr[],int n){
-//     for(int i=0;i<n-1;i++){
-//         for(int j=0;j<n-i-1;j++){
-//             if(arr[j] > arr[j+1]){
-//                 int new = arr[j];
-//                 arr[j] = arr[j+1];
-//                 arr[j+1] = new;
-//             }
-//         }
-//     }
-// }
-
-// int main(){
-//     int n;
-//     scanf("%d",&n);
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         scanf("%d",&arr[i]);
-//     }
-//     check(arr,n);
-//     if(n<2){
-//         printf("-1");
-//     }
-//     printf("%d\n",arr[1]);
-//     return 0;#include<stdio.h>
-
-int check(int arr[], int n) {
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = 0; j < n - i - 1; j++) {
-            if(arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+int check(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j] > arr[j+1]){
+                int new = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = new;
             }
         }
     }
 }
 
-int main() {
+int main(){
     int n;
-    scanf("%d", &n);  // Read number of elements in the array
-    
-    if (n < 2) {
-        printf("-1\n");  // If there are less than 2 elements, print -1
-        return 0;
+    scanf("%d",&n);
+     if(n<2){
+        printf("-1");
     }
-
     int arr[n];
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);  // Read each element
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-
-    check(arr, n);  // Sort the array
-    
-    // After sorting, the second smallest element will be at index 1
-    printf("%d\n", arr[1]);  // Print the second smallest element
+    check(arr,n);
+    printf("%d",arr[1]);
     return 0;
 }
