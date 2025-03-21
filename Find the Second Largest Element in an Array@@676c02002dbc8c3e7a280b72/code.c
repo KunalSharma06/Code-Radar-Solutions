16c4e5d88b354d902dd1ac4127f5e2ed7102d421
Kunal Sharma <sharmakunal19806@gmail.com>
2025-03-21 20:14:06 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,27 +1,53 @@
 #include<stdio.h>
-int srtarr(int arr[],int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                int new = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = new;
+
+void srtarr(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];  // 'new' was changed to 'temp'
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
-int main(){
+
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    for(int i = 0;i<n;i++){
-        scanf("%d",&arr[i]);
+    
+    // Input array elements
+    for (int i = 0; i < n; i++) {  // Fixed loop to go from 0 to n-1
+        scanf("%d", &arr[i]);
     }
-    srtarr(arr,n);
-    if(n==1 || (arr[n-2] == arr[n-1])){
+
+    srtarr(arr, n);
+
+    // Handle edge case where n == 1
+    if (n == 1) {
         printf("-1");
-    } else{
-        printf("%d",arr[n-2]);
+        return 0;
     }
+
+    // Find second largest distinct element
+    int first = arr[n - 1];  // The largest element (after sorting)
+    int second = -1;
+
+    // Start searching from the second last element
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] != first) {
+            second = arr[i];
+            break;  // Break once the second largest is found
+        }
+    }
+
+    // If second remains -1, it means no second distinct element was found
+    if (second == -1) {
+        printf("-1");
+    } else {
+        printf("%d", second);
+    }
+
     return 0;
 }
