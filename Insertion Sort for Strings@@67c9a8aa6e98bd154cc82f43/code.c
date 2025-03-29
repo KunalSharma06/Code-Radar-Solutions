7f2a9bcc86c549d688f89fdbc28458c55b43921c
Kunal Sharma <sharmakunal19806@gmail.com>
2025-03-29 22:50:37 +0530

    Solution to Insertion Sort for Strings

--- Insertion Sort for Strings@@67c9a8aa6e98bd154cc82f43/code.c
+++ Insertion Sort for Strings@@67c9a8aa6e98bd154cc82f43/code.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 // Function to implement bubble sort for strings
-void bubbleSort(char arr[][100], int n) {
+void insertionSort(char arr[][100], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (strcmp(arr[j], arr[j + 1]) > 0) {
