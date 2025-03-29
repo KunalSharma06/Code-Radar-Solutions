fdbd6f15972de325a9ca8b55957278591e0099a3
Kunal Sharma <sharmakunal19806@gmail.com>
2025-03-29 22:51:22 +0530

    Solution to Insertion Sort for Strings

--- Insertion Sort for Strings@@67c9a8aa6e98bd154cc82f43/code.c
+++ Insertion Sort for Strings@@67c9a8aa6e98bd154cc82f43/code.c
@@ -1,24 +1,19 @@
-#include <stdio.h>
-#include <string.h>
-
-// Function to implement bubble sort for strings
-void insertionSort(char arr[][100], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (strcmp(arr[j], arr[j + 1]) > 0) {
-                // Swap arr[j] and arr[j+1]
+#include<string.h>
+int insertionSort(char arr[][100], int n){
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; j < n-i-1; j++){
+            if(strcmp(arr[j],arr[j+1]) > 0){
                 char temp[100];
-                strcpy(temp, arr[j]);
-                strcpy(arr[j], arr[j + 1]);
-                strcpy(arr[j + 1], temp);
+                strcpy(temp,arr[j]);
+                strcpy(arr[j],arr[j+1]);
+                strcpy(arr[j],temp);
             }
         }
     }
 }
 
-// Function to print the array of strings
-void printArray(char arr[][100], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%s\n", arr[i]);
+int printArray(char arr[][100], int n){
+    for(int i = 0; i < n; i++){
+        printf("%s\n",arr[i]);
     }
 }
