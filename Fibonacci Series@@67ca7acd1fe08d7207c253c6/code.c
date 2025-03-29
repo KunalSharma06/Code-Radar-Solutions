92c92f7a033184da3b1c9d1a7be5c610203398e9
Kunal Sharma <sharmakunal19806@gmail.com>
2025-03-29 23:33:39 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,6 +1,6 @@
 int fibonacciSeries(int n){
     if(n >= 1) printf("0 ");
-    if(n >= 2) printf("1");
+    if(n >= 2) printf("1 ");
     int a = 0, b = 1;
     int next;
     for(int i = 3; i <= n; i++){
